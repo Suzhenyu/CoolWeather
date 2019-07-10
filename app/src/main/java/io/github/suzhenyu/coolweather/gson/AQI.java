2813ef508cd29445983ea3bc0eec98bafe5c78f3
suzhenyu <983424749@qq.com>
2019-07-10 17:02:48 +0800

    定义 GSON 实体类

---
+++ app/src/main/java/io/github/suzhenyu/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package io.github.suzhenyu.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/io/github/suzhenyu/coolweather/gson/Weather.java
@@ -0,0 +1,22 @@
+package io.github.suzhenyu.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Weather {
+
+    public String status;
+
+    public Basic basic;
+
+    public AQI aqi;
+
+    public Now now;
+
+    public Suggestion suggestion;
+
+    @SerializedName("daily_forecase")
+    public List<Forecast> forecastList;
+
+}
